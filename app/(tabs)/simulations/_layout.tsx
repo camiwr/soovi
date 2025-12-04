cb908da4970a3e3dc815ca9f59fb11374ce65d3a
camiwr <camillasoares818@gmail.com>
2025-12-03 22:44:25 -0300

    refactor: ajusta opções de cabeçalho nas telas de simulação

--- app/(tabs)/simulations/_layout.tsx
+++ app/(tabs)/simulations/_layout.tsx
@@ -2,27 +2,12 @@ import { Stack } from 'expo-router';
 
 export default function SimulationLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: 'Simulações' }}
-      />
-      <Stack.Screen
-        name="select-area"
-        options={{ title: 'Selecionar área' }}
-      />
-      <Stack.Screen
-        name="create"
-        options={{ title: 'Nova simulação' }}
-      />
-      <Stack.Screen
-        name="[id]"
-        options={{ title: 'Detalhes da simulação' }}
-      />
-      <Stack.Screen
-        name="edit/[id]"
-        options={{ title: 'Editar simulação' }}
-      />
+    <Stack screenOptions={{ headerShown: true }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="select-area" options={{ title: 'Selecionar área' }} />
+      <Stack.Screen name="create" options={{ title: 'Nova simulação' }} />
+      <Stack.Screen name="[id]" options={{ title: 'Detalhes da simulação' }} />
+      <Stack.Screen name="edit/[id]" options={{ title: 'Editar simulação' }} />
     </Stack>
   );
 }
